36fd7ecc8a61ec9d74d79640fb3788feac439371
kokoro <ice.choco.pudding.kokoro@gmail.com>
2018-07-06 00:21:06 +0900

    0705 last commit

--- Programs/Naive_Bayes.py
+++ Programs/Naive_Bayes.py
@@ -1,22 +1,32 @@
 import glob
 import random
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
 
-category={
+
+
+def load_file(yato):
+    # yato=Old_yato or new hoge
+    category={
     'old_fixed_matubi':1,
     'new_fixed_matubi':2,
-}
+    }
 
-docs=[]
-labels=[]
+    docs=[]
+    labels=[]
 
-def load_file():
+
+    
     for c_name,c_id in category.items():
         i=0
-        files=glob.glob('./{}/{}_{}.txt'.format(c_name,i,c_name))
+        files=glob.glob('../Docments/{}/old_fixed_matubi/*.txt'.format(yato))
+        print(files)
         text = ''
         for file in files:
             with open(file, 'r',encoding="utf-8_sig") as f:
                 text=f.read()
+                print(text)
         docs.append(text)
         print(docs)
         labels.append(c_id)
@@ -27,7 +37,7 @@ def load_file():
 
     random.seed()
 
-docs, labels = load_file()  
+docs, labels = load_file('Old_yato')  
 
 indices = list(range(len(docs)))
 random.shuffle(indices)
@@ -38,13 +48,12 @@ test_data    = [docs[i] for i in indices[30:]]
 test_labels  = [labels[i] for i in indices[30:]]
 
 print(train_data)
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.ensemble import RandomForestClassifier
+
 
 #ベクトル化する
 vectorizer = TfidfVectorizer()
 train_matrix = vectorizer.fit_transform(train_data)
+test_matrix = vectorizer.transform(test_data)
 #ナイブベイズ
 clf = MultinomialNB()
 clf.fit(train_matrix, train_labels)
